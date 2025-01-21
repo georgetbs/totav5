e71b09ef63ee317331c1fd265b001a408d794766
georgetbs <georgekvirkvelia@gmail.com>
2025-01-21 13:36:50 +0400

    1.9.9.9.9

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -40,7 +40,7 @@ export default function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
-        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
+        <link rel="icon" href="/favicon.ico?v=20" sizes="any" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
