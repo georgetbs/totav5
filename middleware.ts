cce2f2f7c2c17ca732b719247c1c899f7ffb11e1
georgetbs <georgekvirkvelia@gmail.com>
2025-01-23 13:56:43 +0400

    2.0.0.1

--- middleware.ts
+++ middleware.ts
@@ -45,6 +45,8 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
 }
+
+
 
